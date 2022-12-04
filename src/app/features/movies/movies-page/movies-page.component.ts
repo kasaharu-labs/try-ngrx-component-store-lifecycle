a4a4f6b2d35ac320cf4ab29988dd6a41ffc9c720
Wataru.Kasahara <kasahara1019@gmail.com>
2022-12-04 10:58:44 +0900

    feat: コンポーネント破棄時に subscription の購読を解除する

--- src/app/features/movies/movies-page/movies-page.component.ts
+++ src/app/features/movies/movies-page/movies-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { MovieService } from '../movie.service';
 import { MoviesStore } from './movies.store';
 
@@ -12,17 +13,24 @@ import { MoviesStore } from './movies.store';
   `,
   providers: [MoviesStore],
 })
-export class MoviesPageComponent implements OnInit {
+export class MoviesPageComponent implements OnInit, OnDestroy {
   constructor(
     private readonly store: MoviesStore,
     private readonly service: MovieService
   ) {}
 
   movies$ = this.store.movies$;
+  private onDestroy$ = new Subject<void>();
 
   ngOnInit(): void {
     this.service
       .getMovie('#001')
+      .pipe(takeUntil(this.onDestroy$))
       .subscribe((movie) => this.store.addMovie(movie));
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }

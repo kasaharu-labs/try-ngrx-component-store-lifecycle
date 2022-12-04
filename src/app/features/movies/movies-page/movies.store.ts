722732daf8188ae1b3bc405f8e79ca1be09649c0
Wataru.Kasahara <kasahara1019@gmail.com>
2022-12-04 09:57:40 +0900

    feat: MoviesStore を用意

---
+++ src/app/features/movies/movies-page/movies.store.ts
@@ -0,0 +1,19 @@
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { Movie } from './movie';
+
+export interface MovieState {
+  movies: Movie[];
+}
+
+const initialState: MovieState = {
+  movies: [],
+};
+
+export class MoviesStore extends ComponentStore<MovieState> {
+  constructor() {
+    super(initialState);
+  }
+
+  readonly movies$: Observable<Movie[]> = this.select((state) => state.movies);
+}

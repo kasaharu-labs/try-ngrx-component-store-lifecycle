471009a0d799b6bdd2315d2a10370732a33b3391
Wataru.Kasahara <kasahara1019@gmail.com>
2022-12-04 10:27:14 +0900

    feat: 渡した id の movie を返すメソッドを実装

--- src/app/features/movies/movie.service.ts
+++ src/app/features/movies/movie.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Movie } from './movie';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-
-  constructor() { }
+  getMovie(id: string): Observable<Movie> {
+    return of({ id, name: `movie-${id}` });
+  }
 }
